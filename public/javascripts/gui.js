32dba47eb8914f114bbfac8aa01d1046cf702748
vizam <vizamdevelop@gmail.com>
2021-02-03 01:16:42 -0400

    Minos tweks, all fine

--- public/javascripts/gui.js
+++ public/javascripts/gui.js
@@ -1,21 +1,89 @@
 /**
- * GUI TABs layout
- * 
- * 
- * @param {object} event, onclick
- * @param {string} div id to be displayed or hidded
+ *  
  */
-function changeTab(event, section) {
-  let x, i, tablink;
-  x = document.getElementsByClassName('section');
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = 'none';
+function paginarNotas() {
+  let notas = document.querySelectorAll('#divNotas > div');
+  notas[notas.length - 1].style.display = 'block';
+  let paginas = document.querySelectorAll('#paginador > a');
+  paginas[notas.length - 1].classList.add('w3-green');
+}
+/**
+ * @param {object} event object
+ */
+function cambiarPagina(event) {
+  let pagina = event.target.innerHTML - 1;
+  let notas = document.querySelectorAll('#divNotas > div');
+  let paginador = document.querySelectorAll('#paginador > a');
+  
+  for (let x of notas) {
+    x.style.display = 'none';
   }
-  document.getElementById(section).style.display = "block";
-
-  tablink = document.getElementsByClassName('tablink');
-  for (i = 0; i < tablink.length; i++) {
-    tablink[i].classList.remove('w3-theme-l3');
+  notas[pagina].style.display = 'block';
+  for (let x of paginador) {
+    x.classList.remove('w3-green');
   }
-  event.currentTarget.classList.add('w3-theme-l3');
+  paginador[pagina].classList.add('w3-green');
+}
+/**
+ * 
+ */
+function getAction(event) {
+  let form = document.forms[0];
+  let id = document.querySelector('form > input').value;
+  form.action = `/records`;
+}
+/**
+ * @param {object} e: event from onsubmit
+ */
+function almacenarDatosProfesionales(e) {
+  e.preventDefault();
+  let elementos = e.target.elements;
+  for (let x of elementos) {
+    localStorage.setItem(x.name, x.value);
+  }
+  panelInformativo();
+  //poblarDatosProfesionales();
+  //tagInformativa('#tagExito');
+}
+/**
+ * from localStorate, populate patient report foot section
+ */
+function poblarDatosProfesionales() {
+  let elementos = document.querySelectorAll('#datosProfesionales input[name]');
+  for (let x of elementos) {
+    x.value = localStorage.getItem(x.name) ?? "";
+  }
+  /* let grado = localStorage.getItem("grado") ?? "";
+  let nombres = localStorage.getItem("nombres") ?? "";
+  let apellidos = localStorage.getItem("apellidos") ?? "";
+  let credencial1 = localStorage.getItem("credencial1") ?? '';
+  let credencial2 = localStorage.getItem("credencial2") ?? '';
+  let credencial3 = localStorage.getItem("credencial3") ?? '';
+  let credenciales = `${credencial1} ${credencial2} ${credencial3}`;
+  document.querySelector("#medicoNombre").innerHTML = `${grado} ${nombres} ${apellidos}`;
+  document.querySelector("#medicoEspecialidad").innerHTML = localStorage.getItem("especialidad") ?? "";
+  document.querySelector("#medicoCredenciales").innerHTML = credenciales; */
+}
+/**
+ * 
+ */
+function panelInformativo() {
+  let panel = document.querySelector('#panelExito');
+  let parrafo = document.querySelector('#panelExito > p');
+  parrafo.innerHTML = 'Operación realiazada con éxito';
+  panel.style.display = 'block';
+  retirarPanel(panel);
+}
+/**
+ * @param {object} panel: DOM element
+ * 
+ */
+function retirarPanel(panel) {
+  setTimeout(function() {
+    panel.style.opacity = 0;
+  }, 5000);
+  setTimeout(function () {
+    panel.style.display = "none";
+    panel.style.opacity = 1;
+  }, 7000);
 }
